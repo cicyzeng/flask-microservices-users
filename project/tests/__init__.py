19c06477d1021129eaa71334d54b755314dc690e
cicyzeng <cicyzeng@amazon.com>
2017-10-26 09:45:37 -0700

    react setup

--- project/tests/__init__.py
+++ project/tests/__init__.py
@@ -1,1 +1,23 @@
 # project/tests/__init__.py
+from flask_cors import CORS
+
+def create_app():
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.views import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    return app
